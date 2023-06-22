05d1bfdcdd681d1c8e41396a8867fcc9a843de70
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-06-22 10:26:38 -0600

    Create update_register.sql

---
+++ Introduccion a SQL con MySQL/update_register.sql
@@ -0,0 +1,7 @@
+UPDATE tb_productos SET precio = 28.51 
+WHERE producto = '695594';
+
+UPDATE tb_productos SET sabor = 'Lima/Limón', precio = 4.90
+WHERE producto = '1041119';
+
+SELECT * FROM tb_productos
