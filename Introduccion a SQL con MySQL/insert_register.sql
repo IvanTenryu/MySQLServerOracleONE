b3f53d90f88941bbf236ee48be2d860e42868d08
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-06-22 10:07:56 -0600

    Update insert_register.sql

--- Introduccion a SQL con MySQL/insert_register.sql
+++ Introduccion a SQL con MySQL/insert_register.sql
@@ -1,9 +1,14 @@
 INSERT INTO tb_productos(
 producto, nombre, envase, volumen, sabor,
-precio) VALUES ('695594', 'Festival de Sabores', 'Botella PET',
-'1.5 Litros', 'Asaí', 18.51);
+precio) VALUES ('838819', 'Clean', 'Botella PET',
+'1.5 Litros', 'Naranja', 12.01);
 
 INSERT INTO tb_productos(
 producto, nombre, envase, volumen, sabor,
-precio) VALUES ('1041119', 'Línea Citrus', 'Botella de Vidrio',
-'700 ml', 'Lima', 4.90);
+precio) VALUES ('1037797', 'Clean', 'Botella PET',
+'2 Litros', 'Naranja', 16.01);
+
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('812829', 'Clean', 'Lata',
+'350 ml', 'Naranja', 2.81);
