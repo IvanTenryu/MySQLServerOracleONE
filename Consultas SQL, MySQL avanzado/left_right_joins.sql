4ba97b27960c7a89e512916122972994e270f3ae
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-06-27 14:36:01 -0600

    Update left_right_joins.sql

--- Consultas SQL, MySQL avanzado/left_right_joins.sql
+++ Consultas SQL, MySQL avanzado/left_right_joins.sql
@@ -8,4 +8,11 @@ ON a.DNI = b.DNI;
 SELECT DISTINCT a.DNI, a.NOMBRE, a.CIUDAD, b.DNI
 FROM tabla_de_clientes a
 LEFT JOIN facturas b
-ON a.DNI = b.DNI;
+ON a.DNI = b.DNI
+WHERE b.DNI IS NULL;
+
+SELECT DISTINCT b.DNI, b.NOMBRE, b.CIUDAD, a.DNI
+FROM facturas a
+RIGHT JOIN tabla_de_clientes b
+ON a.DNI = b.DNI
+WHERE a.DNI IS NULL;
