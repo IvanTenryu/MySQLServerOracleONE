75eac29a7f8c0f35ca62b5eaba87b383e13d6fb7
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-06-28 14:38:58 -0600

    Update funciones_date.sql

--- Consultas SQL, MySQL avanzado/funciones_date.sql
+++ Consultas SQL, MySQL avanzado/funciones_date.sql
@@ -4,3 +4,18 @@ SELECT CURRENT_TIMESTAMP();
 
 SELECT DATEDIFF('2023-06-28','1986-04-03')/365;
 
+SELECT current_timestamp() AS hoy,
+date_sub(current_timestamp(), INTERVAL 1 MONTH) as resultado;
+
+select distinct FECHA_VENTA,
+dayname(FECHA_VENTA) as día, 
+monthname(FECHA_VENTA) as mes, 
+year(FECHA_VENTA) as año
+from facturas;
+
+SELECT NOMBRE, 
+TIMESTAMPDIFF(YEAR, FECHA_DE_NACIMIENTO, CURDATE()) AS EDAD
+FROM  tabla_de_clientes;
+
+
+
