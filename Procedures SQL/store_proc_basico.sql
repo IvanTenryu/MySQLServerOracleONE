f748921542b7e2a81f5bdfaf9d26b7222b7cf4d3
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-13 13:03:36 -0600

    Update store_proc_basico.sql

--- Procedures SQL/store_proc_basico.sql
+++ Procedures SQL/store_proc_basico.sql
@@ -9,3 +9,13 @@ CALL concatenar;
 CALL concatenar_comentarios;
 
 CALL concatenar2;
+
+CALL exhibir_variable;
+
+CALL multi_declaracion;
+
+CALL sin_declaracion;
+
+CALL atribuir_valores;
+
+CALL desafio_1;
