fc9b68bfbcdf135c354c193fc4c4fac2bea8c84e
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-18 13:59:20 -0600

    Update if_elseif.sql

--- Procedures SQL/if_elseif.sql
+++ Procedures SQL/if_elseif.sql
@@ -1,1 +1,7 @@
 SELECT * FROM tabla_de_productos;
+
+SELECT PRECIO_DE_LISTA FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = '1000800';
+
+CALL precio_producto('1000801');
+
+CALL precio_producto('1013793');
