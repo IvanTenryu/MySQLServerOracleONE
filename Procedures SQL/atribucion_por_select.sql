3607fafead1c61f5bf3a658969994f410597b3a8
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-18 13:01:03 -0600

    Create atribucion_por_select.sql

---
+++ Procedures SQL/atribucion_por_select.sql
@@ -0,0 +1,7 @@
+SELECT * FROM tabla_de_productos;
+
+SELECT * FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = '1078680';
+
+SELECT CODIGO_DEL_PRODUCTO, SABOR FROM tabla_de_productos;
+
+CALL mostrar_sabor('1101035');
