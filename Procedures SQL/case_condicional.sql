7976ea60fa89c8f8a5e0fc67b0c03a6b7976dd65
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-18 15:19:30 -0600

    Update case_condicional.sql

---
+++ Procedures SQL/case_condicional.sql
@@ -0,0 +1,28 @@
+DELIMITER $$
+CREATE PROCEDURE `comparacion_ventas_case`(vfecha1 DATE, vfecha2 DATE)
+BEGIN
+DECLARE facturacion_inicial FLOAT;
+DECLARE facturacion_final FLOAT;
+DECLARE variacion float;
+DECLARE mensaje VARCHAR(20);
+SELECT SUM(B.CANTIDAD * B.PRECIO) INTO facturacion_inicial  FROM 
+facturas A INNER JOIN items_facturas B
+ON A.NUMERO = B.NUMERO
+WHERE A.FECHA_VENTA = vfecha1;
+SELECT SUM(B.CANTIDAD * B.PRECIO) INTO facturacion_final  FROM 
+facturas A INNER JOIN items_facturas B
+ON A.NUMERO = B.NUMERO
+WHERE A.FECHA_VENTA = vfecha2;
+SET variacion = ((facturacion_final / facturacion_inicial) -1) * 100;
+	CASE
+	WHEN variacion > 10 
+		THEN SET mensaje = 'Verde';
+	WHEN variacion >= -10 AND variacion <= 10 
+		THEN SET mensaje = 'Amarillo';
+	WHEN varacion < -10 
+		THEN SET mensaje = 'Rojo';
+	END CASE;
+SELECT mensaje;
+END $$
+
+CALL comparacion_ventas_case(20150101, 20150102);
