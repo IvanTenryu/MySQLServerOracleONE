00b2be2e14f69b2e835ae25282eab43e924af4ac
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-18 15:17:32 -0600

    Update precio_producto_case.sql

--- Procedures SQL/precio_producto_case.sql
+++ Procedures SQL/precio_producto_case.sql
@@ -5,10 +5,18 @@ BEGIN
     DECLARE vprecio FLOAT;
     SELECT PRECIO_DE_LISTA INTO vprecio
     FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = vcodigo;
-		IF vprecio >= 12 THEN SET vresultado = 'Producto costoso';
-		ELSEIF vprecio >= 7 AND vprecio < 12 THEN SET vresultado = 'Producto asequible';
-		ELSE SET vresultado = 'Producto barato';
-		END IF;
+		CASE
+        WHEN vprecio >= 12
+			THEN SET vresultado = 'Producto costoso';
+		WHEN vprecio >= 7 AND vprecio < 12 
+			THEN SET vresultado = 'Producto asequible';
+		WHEN vprecio < 7 
+			THEN SET vresultado = 'Producto barato';
+		END CASE;
 	SELECT vresultado;
 END
 $$
+
+CALL precio_producto_case('1000801');
+CALL precio_producto_case('1013793');
+CALL precio_producto_case('1096818');
