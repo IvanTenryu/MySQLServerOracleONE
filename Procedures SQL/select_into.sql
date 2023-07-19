b3b4d281a0a20dc721066fa67d6c9a263e3a0bde
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-19 09:10:19 -0600

    Create select_into.sql

---
+++ Procedures SQL/select_into.sql
@@ -0,0 +1,10 @@
+CALL problema_select_into;
+
+/*
+	No me sirve SELECT INTO. Entonces, ahí existe una estructura
+    también llamada cursor. Y esta estructura sí nos permite 
+    tomar valor de un array y almacenarlo en un array o un cursor 
+    es como una red ya vacío que está disponible para que nosotros 
+    podamos trabajar sobre él, pues podemos hacer ciclos de lazo.
+*/
+
