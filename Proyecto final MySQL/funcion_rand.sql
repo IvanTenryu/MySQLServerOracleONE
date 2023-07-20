80b7fdb6b85cee421daf9cb13da6d2a7dfabf37d
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-20 13:16:03 -0600

    Update funcion_rand.sql

--- Proyecto final MySQL/funcion_rand.sql
+++ Proyecto final MySQL/funcion_rand.sql
@@ -8,6 +8,18 @@ SELECT (RAND()*(250-20+1))+20 AS ALEATORIO;
 
 SELECT FLOOR((RAND()*(250-20+1))+20) AS ALEATORIO;
 
+/* Inicio de función para número aleatorio */
+DELIMITER $$
+USE `empresa`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `f_aleatorio`(min INT, max INT) RETURNS int
+BEGIN
+	DECLARE vAresultado INT;
+    SELECT FLOOR((RAND()*(max-min+1))+min)
+		INTO vresultado;
+RETURN vresultado;
+END$$
+/* Fin de función para número aleatorio */
+
 SELECT f_aleatorio(1,10) AS RESULTADO;
 
 
