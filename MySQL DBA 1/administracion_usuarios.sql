e8cbb9ea4b890cc0b782884150c3ac5ff2ac49d9
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2023-07-28 13:03:06 -0600

    Update administracion_usuarios.sql

--- MySQL DBA 1/administracion_usuarios.sql
+++ MySQL DBA 1/administracion_usuarios.sql
@@ -1,16 +1,18 @@
 -- Usuario administrador de la base de datos
 CREATE USER 'admin02'@'localhost' IDENTIFIED BY 'admin02';
-
 GRANT ALL PRIVILEGES ON *.* TO 'admin02'@'localhost' WITH GRANT OPTION;
 
 -- Usuario normal
 CREATE USER 'user02'@'localhost' IDENTIFIED BY 'user02';
-
 GRANT SELECT, UPDATE, INSERT, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE
 ON *.* TO 'user02'@'localhost';
 
+-- Usuario de solo lectura
+CREATE USER 'lector01'@'localhost' IDENTIFIED BY 'lector01';
+GRANT SELECT, EXECUTE ON *.* TO 'lector01'@'localhost';
 
-
-
+-- Usuario para respaldos
+CREATE USER 'back01'@'localhost' IDENTIFIED BY 'back01';
+GRANT SELECT, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'back01'@'localhost';
 
 
